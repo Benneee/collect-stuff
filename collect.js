d184916670ccb47626630cc5dd97a0c400bffd18
Benedict Nkeonye <benedictiknkeonye@gmail.com>
2020-02-22 18:11:48 +0100

    Added cron job

--- collect.js
+++ collect.js
@@ -1,6 +1,7 @@
 const puppeteer = require("puppeteer");
 const { getSavedEpisode, saveEpisodeInfo } = require("./saveAndEmail");
 const nodemailer = require("nodemailer");
+const cron = require("node-cron");
 
 const log = console.log;
 const episode1 = "Chicago-Med-8";
@@ -27,44 +28,50 @@ async function collectEpisodeInfo(url) {
   browser.close();
 }
 
-collectEpisodeInfo(`https://o2tvseries.com/${episode1}/Season-05/index.html`)
-  .then(() => {
-    const episode = getSavedEpisode();
-    const { episodeTitle, episodeLink } = episode;
+cron.schedule("* * * * Friday", () => {
+  log("Running cron job now");
 
-    let transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 465,
-      secure: true,
-      auth: {
-        user: process.env.MAIL,
-        pass: process.env.MAILPASSWORD
-      }
-    });
+  collectEpisodeInfo(`https://o2tvseries.com/${episode1}/Season-05/index.html`)
+    .then(() => {
+      const episode = getSavedEpisode();
+      const { episodeTitle, episodeLink } = episode;
 
-    let mailOptions = {
-      from: process.env.MAIL,
-      to: "benedictiknkeonye@gmail.com",
-      subject: `New ${episode1} has just been released`,
-      html: `<p>Hiya champ! There's a new episode of ${seriesName}.</p> <br />
+      let transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
+        auth: {
+          user: process.env.MAIL,
+          pass: process.env.MAILPASSWORD
+        }
+      });
+
+      let mailOptions = {
+        from: process.env.MAIL,
+        to: "benedictiknkeonye@gmail.com",
+        subject: `New ${episode1} has just been released`,
+        html: `<p>Hiya champ! There's a new episode of ${seriesName}.</p> <br />
       Download ${episodeTitle} <a href=${episodeLink}>here</a>`
-    };
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        log("error: ", error);
-      } else {
-        log("info: ", info);
-      }
+      };
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          log("error: ", error);
+        } else {
+          log("info: ", info);
+        }
+      });
+    })
+    .catch(err => {
+      log(err);
     });
-  })
-  .catch(err => {
-    log(err);
-  });
+});
 
 /**
  * ToDos
+ * Check for difference in episode before sending mail
  * Create an array of various shows
  * Check that they have the same markup to ensure scraping method can just collect array values as parameters
- * Create the cron job
+ * Deploy env variables to Heroku
+ * Set up the cron job
  * Deploy the script
  */
